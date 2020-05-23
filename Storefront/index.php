77f015f245486c6f0e47456475c19c081e2e1cb8
Brandon Sanchez <brandonbensanchez@gmail.com>
2020-05-23 11:45:52 -0700

    Login screen basic CSS

--- Storefront/index.php
+++ Storefront/index.php
@@ -4,17 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Storefront Login</title>
+        <link href="CSS/reset.css" rel="stylesheet">
+        <link href="CSS/index.css" rel="stylesheet">
     </head>
     <body>
         <h1>Storefront</h1>
-        <h2>Login</h2>
-        <form method="POST" action="Includes/login_inc.php">
-            <p>Username:</p>
-            <input type="text" name="username">
-            <p>Password:</p>
-            <input type="password" name="password">
-            <input type="submit" value="Login" name="login_submit">
-        </form>
-        <a href="signup.php">Sign Up</a>
+        <main>
+            <h2>Login</h2>
+            <div id="form">
+                <form method="POST" action="Includes/login_inc.php">
+                    <p id="username">Username</p>
+                    <input type="text" name="username" class="input">
+                    <p id="password">Password</p>
+                    <input type="password" name="password" class="input"><br>
+                    <input type="submit" value="Login" name="login_submit" id="submit">
+                </form>
+                <a href="signup.php">Sign Up</a>
+            </div>
+        </main>
     </body>
 </html>
