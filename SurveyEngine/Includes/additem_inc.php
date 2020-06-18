a466960db9d94b17fb9c97e0513aa0870a2c0cba
Brandon Sanchez <brandonbensanchez@gmail.com>
2020-06-18 10:20:28 -0700

    Can add questions

--- SurveyEngine/Includes/additem_inc.php
+++ SurveyEngine/Includes/additem_inc.php
@@ -4,82 +4,26 @@ if(isset($_POST['item_submit'])){ //User got here legitimately
     include_once 'dbh_inc.php'; //So it doesn't called called twice
     session_start();
 
-    $item_name = $_POST['item_name'];
-    $description = $_POST['description'];
-    $num_in_stock = $_POST['num_in_stock'];
-    $unit_price = $_POST['unit_price'];
+    $question = $_POST['item_name'];
 
-    $file = $_FILES['file']; //Array of all image data uploaded
-    $is_default = true;
 
-    if($file['error'] === 0){
-            $file_name = $file['name'];
-            $file_type = $file['type'];
-            $file_temp_name = $file['tmp_name'];
-            $file_error = $file['error'];
-            $file_size = $file['size'];
-            $file_ext = explode('.', $file_name); //Splits into array
-            $file_end_ext = strtolower(end($file_ext)); //Gets the end extension name
-            $is_default = false;   
-            $file_ext = explode('.', $file_name); //Splits into array
-            $file_end_ext = strtolower(end($file_ext)); //Gets the end extension name
+    if(empty($question)) {
+        header('Location: ../additems.php?error=emptyfields');
+        exit();
     }
     else {
-        $file_error = 0;
-        $file_size = 0;
-        $file_end_ext = 'jpg';
-        $file_error = 0;
-    }
+        $sql = 'INSERT INTO bsanchez_se_questions VALUES (DEFAULT, ?, ?);'; //(item_id, store_id, name, descr, img_dest, num_in_stock, unit_price)
+        $statement = mysqli_stmt_init($conn);
 
-    $allowed = array('jpg', 'jpeg', 'png');
-
-    if(in_array($file_end_ext, $allowed)) { //If Proper file type
-        if($file_error === 0) { //No errors
-            if($file_size < 1000000) { //Less than 1mb
-                if($is_default) {
-                    $image_full_name = 'default.jpg';
-                }
-                else {
-                    $image_full_name = uniqid('', true) . '.'. $file_end_ext;
-                }
-
-                $file_destination = '../Uploads/Item/' . $image_full_name;
-                if(empty($item_name) || empty($description) || empty($num_in_stock) || empty($unit_price)) {
-                    header('Location: ../additems.php?error=emptyfields');
-                    exit();
-                }
-                else {
-                    $sql = 'INSERT INTO bsanchez_items VALUES (DEFAULT, ?, ?, ?, ?, ?, ?);'; 
-                    //(item_id, store_id, name, descr, img_dest, num_in_stock, unit_price)
-                    $statement = mysqli_stmt_init($conn);
-
-                    if(!mysqli_stmt_prepare($statement, $sql)) { //If statement fails
-                        echo 'SQL Failed';
-                        exit();
-                    }
-                    else {
-                        mysqli_stmt_bind_param($statement, 'issssd', $_SESSION['store_id'], $item_name, 
-                                                $description, $image_full_name, $num_in_stock, $unit_price); //Inputs variables into ?
-                        mysqli_stmt_execute($statement);
-
-                        move_uploaded_file($file_temp_name, $file_destination); //Upload image
-
-                        header('Location: ../additems.php?upload=success');
-                    }
-                }
-            }
-            else {
-                header('Location: ../additems.php?error=sizeerror');
-                exit();
-            }
-        }
-        else {
-            header("Location: ../additems.php?error=file");
+        if(!mysqli_stmt_prepare($statement, $sql)) { //If statement fails
+            echo 'SQL Failed';
             exit();
         }
-    }
-    else {
-        header("Location: ../additems.php?error=fileext");
-        exit();
+        else {
+            mysqli_stmt_bind_param($statement, 'is', $_SESSION['survey_id'], $question); //Inputs variables into ?
+            mysqli_stmt_execute($statement);
+
+            header('Location: ../additems.php?upload=success');
+        }
     }
 }
--- SurveyEngine/additems.php
+++ SurveyEngine/additems.php
@@ -12,7 +12,7 @@
         <?php
             include_once 'includes/dbh_inc.php'; //So it doesn't get called twice
 
-            $sql = 'SELECT * FROM bsanchez_items WHERE survey_id=?;'; //Gets only for user logged in
+            $sql = 'SELECT * FROM bsanchez_se_questions WHERE survey_id=?;'; //Gets only for user logged in
             $statement = mysqli_stmt_init($conn);
 
             if(!mysqli_stmt_prepare($statement, $sql)) {
@@ -24,19 +24,13 @@
                 $result = mysqli_stmt_get_result($statement);
 
                 while($row = mysqli_fetch_assoc($result)) { //Each one is a store card
-                    $format_price = number_format((float)$row['unit_price'], 2, '.', '');
-                    echo '<div class="store_card" id="item_'.$row['item_id'].'">
-                    <h2>'.$row['name'].'</h2>
+                    echo '<div class="store_card" id="item_'.$row['question_id'].'">
+                    <h2>Question</h2>
                     <div class="bottom_card">
-                        <img src="Uploads/Item/'.$row['img_dest'].'" height=80px>
-                        <p class="item_descr top description">'.$row['description'].'</p>
-                        <h3 class="item_titles">Number in Stock</h3>
-                        <p class="item_descr num_in_stock">'.$row['num_in_stock'].'</p>
-                        <h3 class="item_titles">Price</h3>
-                        <p class="item_descr unit_price">'.$format_price.'</p>
-                        <button class="edit_item button" type="button" value='.$row['item_id'].'>Edit Item</button>
+                        <p class="item_descr top description">'.$row['question'].'</p>
+                        <button class="edit_item button" type="button" value='.$row['question_id'].'>Edit Question</button>
                         <form method="POST" action="Includes/deleteitem_inc.php">
-                        <button class="button delete" type="submit" name="delete_item" value='.$row['item_id'].'>Delete Item</button>
+                        <button class="button delete" type="submit" name="delete_item" value='.$row['question_id'].'>Delete Question</button>
                         </form>
                     </div>
                     </div>';
@@ -45,17 +39,9 @@
         ?>
         <div id="add_store">
             <h2>Add Question</h2>
-            <form method="POST" action="Includes/additem_inc.php" enctype="multipart/form-data" class="form">
-                <p class="store_name">Item Name</p>
+            <form method="POST" action="Includes/additem_inc.php" class="form">
+                <p class="store_name">Question</p>
                 <input type="text" name="item_name" class="input">
-                <p class="store_descr">Description</p>
-                <input type="text" name="description" class="input">
-                <p class="store_descr">Quantity</p>
-                <input type="text" name="num_in_stock" class="input">
-                <p class="store_descr">Unit Price</p>
-                <input type="text" name="unit_price" class="input">
-                <p class="add_image">Add Image (Optional)</p>
-                <input type="file" name="file"><br>
                 <input class="button" type="submit" value="Add Item" name="item_submit" id="add_button">
             </form>
         </div>
